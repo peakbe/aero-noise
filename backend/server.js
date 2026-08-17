0595934023b5fe43d55252198f79fac692856fcd
peakbe <xpb66bfnpd@privaterelay.appleid.com>
2026-08-17 10:22:21 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const fetch = require("node-fetch"); // obligatoire en CJS
 
 const app = express();
 
